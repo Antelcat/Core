ba171fc568fae7962fcd161df573754ad60b3e24
Feast <feast107@gmail.com>
2023-05-13 14:15:03 +0800

    Create JsonDateTimeConverter.cs

---
+++ Feast.Foundation/Feast.Foundation.Core/Utils/JsonDateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Feast.Foundation.Extensions;
+
+namespace Feast.Foundation.Utils
+{
+    public class JsonDateTimeConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type _, JsonSerializerOptions __) =>
+            reader.TokenType switch
+            {
+                JsonTokenType.Number => reader.GetInt64().ToLocalTime(),
+                JsonTokenType.String => reader.GetString().ToDateTime(),
+                JsonTokenType.Null => DateTime.MinValue,
+                _ => reader.GetDateTime()
+            };
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            if (value == DateTime.MinValue)
+                writer.WriteNullValue();
+            else writer.WriteNumberValue(value.ToTimestamp());
+        }
+    }
+}
