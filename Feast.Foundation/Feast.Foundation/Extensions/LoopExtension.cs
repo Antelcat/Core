26db60f9684ea4dd24629516b5cc5b9f70c46d92
Feast <feast107@gmail.com>
2023-05-13 13:49:14 +0800

    Create LoopExtension.cs

---
+++ Feast.Foundation/Feast.Foundation/Extensions/LoopExtension.cs
@@ -0,0 +1,13 @@
+﻿namespace Feast.Foundation.Extensions
+{
+    public static class LoopExtension
+    {
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+    }
+}
