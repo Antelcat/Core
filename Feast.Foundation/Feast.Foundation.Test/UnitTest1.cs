ebfa0432027e6b88b256a7d266ff112d9f8e25f2
Feast <feast107@gmail.com>
2023-05-13 13:48:27 +0800

    Create UnitTest1.cs

---
+++ Feast.Foundation/Feast.Foundation.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Feast.Foundation.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
