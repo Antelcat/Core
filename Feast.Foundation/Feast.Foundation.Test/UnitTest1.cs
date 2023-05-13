6194c543b711f984a6c4de7b2dbed1d6c9a7d1b1
Feast <feast107@gmail.com>
2023-05-13 14:16:05 +0800

    Update UnitTest1.cs

--- Feast.Foundation/Feast.Foundation.Test/UnitTest1.cs
+++ Feast.Foundation/Feast.Foundation.Test/UnitTest1.cs
@@ -1,19 +1,39 @@
-using System.Dynamic;
-using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Feast.Foundation.Test
 {
     public class Tests
     {
+        public interface IDemo
+        {
+            string Method();
+        }
+
+        public class Demo : IDemo
+        {
+            private readonly string message = "This is a demo";
+            public Demo(object iNeedSomething) { }
+            public string Method()
+            {
+                Console.WriteLine(message);
+                return message;
+            }
+        }
+
+
+        IServiceCollection collection = new ServiceCollection();
+        private IServiceProvider provider;
         [SetUp]
         public void Setup()
         {
-            var proxy = DispatchProxy.Create<ExpandoObject,DispatchProxy>();
+            collection.AddTransient<IDemo, Demo>();
+            provider = collection.BuildServiceProvider();
         }
 
         [Test]
         public void Test1()
         {
+            provider.GetRequiredService<IDemo>().Method();
             Assert.Pass();
         }
     }
