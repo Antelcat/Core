4fde39ea3c5377b4e56b6269f4a7b063a69b82b6
Feast <feast107@gmail.com>
2023-05-13 21:39:27 +0800

    Update StringExtension.cs

--- Feast.Foundation/Feast.Foundation.Core/Extensions/StringExtension.cs
+++ Feast.Foundation/Feast.Foundation.Core/Extensions/StringExtension.cs
@@ -13,19 +13,31 @@
                     >= 'A' and <= 'Z' => value.ToLowerCamelCase(),
                     _ => throw new ArgumentOutOfRangeException($"First char should be letter but {value[0]}")
                 };
+
         public static bool IsNullOrEmpty(this string? str) => string.IsNullOrEmpty(str);
         public static bool IsNullOrWhiteSpace(this string? str) => string.IsNullOrWhiteSpace(str);
-        public static int ToInt(this string? str) =>
-            int.TryParse(str, out var result) ? result : 0;
-        public static long ToLong(this string? str) => 
-            long.TryParse(str, out var result) ? result : 0L;
-        public static bool ToBool(this string? str) =>
-            bool.TryParse(str, out var result) && result;
-        public static float ToFloat(this string? str) =>
-            float.TryParse(str, out var result) ? result : float.NaN;
-        public static double ToDouble(this string? str) =>
-            double.TryParse(str, out var result) ? result : double.NaN;
-        public static DateTime ToDateTime(this string? str) => 
-            DateTime.TryParse(str, out var result) ? result : DateTime.MinValue;
+
+        public static bool ToByte(this string? str, out byte result) => byte.TryParse(str, out result);
+        public static bool ToBool(this string? str, out bool result) => bool.TryParse(str, out result);
+        public static bool ToShort(this string? str, out short result) => short.TryParse(str, out result);
+        public static bool ToUShort(this string? str, out ushort result) => ushort.TryParse(str, out result);
+        public static bool ToInt(this string? str, out int result) => int.TryParse(str, out result);
+        public static bool ToUInt(this string? str, out uint result) => uint.TryParse(str, out result);
+        public static bool ToLong(this string? str, out long result) => long.TryParse(str, out result);
+        public static bool ToULong(this string? str, out ulong result) => ulong.TryParse(str, out result);
+        public static bool ToFloat(this string? str, out float result) => float.TryParse(str, out result);
+        public static bool ToDouble(this string? str, out double result) => double.TryParse(str, out result);
+        public static bool ToDateTime(this string? str, out DateTime result) => DateTime.TryParse(str, out result);
+        public static byte ToByte(this string? str) => str.ToByte(out var result) ? result : byte.MinValue;
+        public static bool ToBool(this string? str) => str.ToBool(out var result) && result;
+        public static short ToShort(this string? str) => str.ToShort(out var result) ? result : short.MinValue;
+        public static ushort ToUShort(this string? str) => str.ToUShort(out var result) ? result : ushort.MinValue;
+        public static int ToInt(this string? str) => str.ToInt(out var result) ? result : int.MinValue;
+        public static uint ToUInt(this string? str) => str.ToUInt(out var result) ? result : uint.MinValue;
+        public static long ToLong(this string? str) => str.ToLong(out var result) ? result : long.MaxValue;
+        public static ulong ToULong(this string? str) => str.ToULong(out var result) ? result : ulong.MinValue;
+        public static float ToFloat(this string? str) => str.ToFloat(out var result) ? result : float.NaN;
+        public static double ToDouble(this string? str) => str.ToDouble(out var result) ? result : double.NaN;
+        public static DateTime ToDateTime(this string? str) => str.ToDateTime(out var result) ? result : DateTime.MinValue;
     }
 }
