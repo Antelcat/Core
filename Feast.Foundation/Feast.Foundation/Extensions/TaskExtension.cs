20b8dda337b84f5e5cfdba58ea92b7bc50bcf912
Feast <feast107@gmail.com>
2023-05-13 14:15:51 +0800

    Delete TaskExtension.cs

--- Feast.Foundation/Feast.Foundation/Extensions/TaskExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Feast.Foundation.Extensions
-{
-    public static class TaskExtension
-    {
-        public static TaskAwaiter GetAwaiter(this int delay) => Task.Delay(delay).GetAwaiter();
-    }
-}
