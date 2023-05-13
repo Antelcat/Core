35c42f9e76b031f8468560de718012ff1b0e7a65
Feast <feast107@gmail.com>
2023-05-13 21:39:29 +0800

    Create TypeExtension.cs

---
+++ Feast.Foundation/Feast.Foundation.Core/Extensions/TypeExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace Feast.Foundation.Core.Extensions
+{
+    public static class TypeExtension
+    {
+        public static object RawInstance(this Type type) => FormatterServices.GetSafeUninitializedObject(type);
+        public static T RawInstance<T>() => (T)FormatterServices.GetSafeUninitializedObject(typeof(T));
+        public static object? NewInstance(this Type type) => Activator.CreateInstance(type);
+        public static T? NewInstance<T>() => Activator.CreateInstance<T>();
+    }
+}
