d70945eda8fa385db2b80ebaabe0cd7091b7a638
Feast <feast107@gmail.com>
2023-05-13 14:14:40 +0800

    Create TaskExtension.cs

---
+++ Feast.Foundation/Feast.Foundation.Core/Extensions/TaskExtension.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Feast.Foundation.Extensions
+{
+    public static class TaskExtension
+    {
+        public static TaskAwaiter GetAwaiter(this int delay) => Task.Delay(delay).GetAwaiter();
+    }
+}
