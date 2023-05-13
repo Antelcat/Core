4a7f7bad78873900b75ca7cc6171783a8a0c3519
Feast <feast107@gmail.com>
2023-05-13 14:15:12 +0800

    Delete IStateMachine.cs

--- Feast.Foundation/Feast.Foundation/Interfaces/IStateMachine.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace Feast.Foundation.Interfaces
-{
-    public interface IStateMachine<out TState> : INotifyPropertyChanged
-    {
-        TState State { get; }
-    }
-}
