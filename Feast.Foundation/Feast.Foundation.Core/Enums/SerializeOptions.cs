ea1d3a9bf39abe24c0650f8aee7be9e8ac7a2869
Feast <feast107@gmail.com>
2023-05-13 14:14:31 +0800

    Create SerializeOptions.cs

---
+++ Feast.Foundation/Feast.Foundation.Core/Enums/SerializeOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Feast.Foundation.Enums
+{
+    [Flags]
+    public enum SerializeOptions
+    {
+        None           = 0b00,
+        EnumToString   = 0b01,
+        DateTimeToLong = 0b10,
+        All            = 0b11,
+    }
+}
