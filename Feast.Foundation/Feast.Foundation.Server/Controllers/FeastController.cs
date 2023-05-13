525947bf0c4faf757c17307ce29b6550a542f77c
Feast <feast107@gmail.com>
2023-05-13 21:39:33 +0800

    Create FeastController.cs

---
+++ Feast.Foundation/Feast.Foundation.Server/Controllers/FeastController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Feast.Foundation.Server.Controllers
+{
+    public abstract class FeastController : Controller
+    {
+        protected FeastController() { }
+
+        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            Debugger.Break();
+            return base.OnActionExecutionAsync(context, next);
+        }
+    }
+}
