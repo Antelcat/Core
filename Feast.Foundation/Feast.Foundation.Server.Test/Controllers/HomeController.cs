1c5e1184b46bc63190c49b708ac7058609767e0f
Feast <feast107@gmail.com>
2023-05-14 15:19:38 +0800

    Create HomeController.cs

---
+++ Feast.Foundation/Feast.Foundation.Server.Test/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Feast.Foundation.Core.Models;
+using Feast.Foundation.Server.Test.Models;
+using Feast.Foundation.Server.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Feast.Foundation.Server.Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    [AllowAnonymous]
+    public class HomeController : Controller
+    {
+        private JwtConfigure<User> configure;
+
+        public HomeController(JwtConfigure<User> configure)
+        {
+            this.configure = configure;
+        }
+
+        [HttpPost]
+        public Response<string> Login([FromBody]User user)
+        {
+            return new Response<string>(configure.CreateToken(user));
+        }
+    }
+}
