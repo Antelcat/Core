e4efb6eb2edf1df87e743bf5f9687bae299ea5bf
Feast <feast107@gmail.com>
2023-05-14 15:18:50 +0800

    Create NoneConverter.cs

---
+++ Feast.Foundation/Feast.Foundation.Core/Implements/Converters/NoneConverter.cs
@@ -0,0 +1,12 @@
+﻿using Feast.Foundation.Core.Interface.Converting;
+
+namespace Feast.Foundation.Core.Implements.Converters
+{
+    public class NoneConverter : IValueConverter
+    {
+        public static readonly NoneConverter Instance = new ();
+
+        public object? To(object? input) => input;
+        public object? Back(object? input) => input;
+    }
+}
