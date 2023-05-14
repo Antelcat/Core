cc198609f22947fed17ec93e249ab47bd9681093
Feast <feast107@gmail.com>
2023-05-14 21:15:44 +0800

    Update TypeExtension.cs

--- Feast.Foundation/Feast.Foundation.Core/Extensions/TypeExtension.cs
+++ Feast.Foundation/Feast.Foundation.Core/Extensions/TypeExtension.cs
@@ -6,17 +6,37 @@ namespace Feast.Foundation.Core.Extensions
 {
     public static class TypeExtension
     {
-        public static object RawInstance(this Type type) => FormatterServices.GetSafeUninitializedObject(type);
-        public static T RawInstance<T>() => (T)FormatterServices.GetSafeUninitializedObject(typeof(T));
+        /// <summary>
+        /// 创建未初始化的对象
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        /// <exception cref="SerializationException"></exception>
+        public static object RawInstance(this Type type) => FormatterServices.GetUninitializedObject(type);
+        /// <summary>
+        /// 创建未初始化的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T RawInstance<T>() => (T)FormatterServices.GetUninitializedObject(typeof(T));
+        /// <summary>
+        /// 创建一个对象
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
         public static object? NewInstance(this Type type) => Activator.CreateInstance(type);
+        /// <summary>
+        /// 创建一个对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public static T? NewInstance<T>() => Activator.CreateInstance<T>();
 
-        public static IValueConverter? Converter(this Type type, Type toType)
-        {
-            return type == toType ? NoneConverter.Instance
-                : type == typeof(string) 
-                ? StringConverterBase.ConvertTo(toType)
-                : null!;
-        }
+        public static IValueConverter Converter(this Type type, Type toType) =>
+            type == toType
+                ? NoneConverter.Instance
+                : type == typeof(string)
+                    ? StringConverterBase.ConvertTo(toType)
+                    : throw new NotSupportedException("Not support this type yet");
     }
 }
