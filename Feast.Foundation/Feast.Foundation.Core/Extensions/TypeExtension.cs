4b040c45b85ad9429d522631f488a7c9bc0ef20f
Feast <feast107@gmail.com>
2023-05-14 15:18:49 +0800

    Update TypeExtension.cs

--- Feast.Foundation/Feast.Foundation.Core/Extensions/TypeExtension.cs
+++ Feast.Foundation/Feast.Foundation.Core/Extensions/TypeExtension.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.Serialization;
+﻿using Feast.Foundation.Core.Implements.Converters;
+using Feast.Foundation.Core.Interface.Converting;
+using System.Runtime.Serialization;
 
 namespace Feast.Foundation.Core.Extensions
 {
@@ -8,5 +10,13 @@ namespace Feast.Foundation.Core.Extensions
         public static T RawInstance<T>() => (T)FormatterServices.GetSafeUninitializedObject(typeof(T));
         public static object? NewInstance(this Type type) => Activator.CreateInstance(type);
         public static T? NewInstance<T>() => Activator.CreateInstance<T>();
+
+        public static IValueConverter? Converter(this Type type, Type toType)
+        {
+            return type == toType ? NoneConverter.Instance
+                : type == typeof(string) 
+                ? StringConverterBase.ConvertTo(toType)
+                : null!;
+        }
     }
 }
