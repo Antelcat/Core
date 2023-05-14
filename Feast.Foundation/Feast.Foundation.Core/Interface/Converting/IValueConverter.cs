6c17a7ebc7430977283190b667c712c67675af85
Feast <feast107@gmail.com>
2023-05-14 15:19:12 +0800

    Create IValueConverter.cs

---
+++ Feast.Foundation/Feast.Foundation.Core/Interface/Converting/IValueConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace Feast.Foundation.Core.Interface.Converting
+{
+    public interface IValueConverter<TIn, TOut>
+    {
+        TOut? To(TIn? input);
+        TIn? Back(TOut? input);
+    }
+
+    public interface IValueConverter : IValueConverter<object, object> { }
+}
