bca026420dc3a30f1d0ca0bf9c9fc12f66178b1f
Feast <feast107@gmail.com>
2023-05-14 15:19:15 +0800

    Create User.cs

---
+++ Feast.Foundation/Feast.Foundation.Server.Test/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Feast.Foundation.Server.Test.Models
+{
+    public class User
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
