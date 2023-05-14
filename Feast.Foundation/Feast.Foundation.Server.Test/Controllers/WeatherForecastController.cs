d9a108b3fdc1118b4dab9037778331e22d1664ad
Feast <feast107@gmail.com>
2023-05-14 15:19:39 +0800

    Update WeatherForecastController.cs

--- Feast.Foundation/Feast.Foundation.Server.Test/Controllers/WeatherForecastController.cs
+++ Feast.Foundation/Feast.Foundation.Server.Test/Controllers/WeatherForecastController.cs
@@ -1,20 +1,28 @@
+using Feast.Foundation.Core.Models;
 using Feast.Foundation.Server.Controllers;
+using Feast.Foundation.Server.Test.Models;
+using Feast.Foundation.Server.Utils;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Feast.Foundation.Server.Test.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
+    [Authorize]
     public class WeatherForecastController : FeastController
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, JwtConfigure<User> configure)
         {
             _logger = logger;
         }
@@ -22,6 +30,7 @@ namespace Feast.Foundation.Server.Test.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var user = Identity<User>();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
