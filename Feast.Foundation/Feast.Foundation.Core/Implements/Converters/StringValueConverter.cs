7f6305f23835244b6f04c376f06483ca085a8806
Feast <feast107@gmail.com>
2023-05-14 21:15:48 +0800

    Update StringValueConverter.cs

--- Feast.Foundation/Feast.Foundation.Core/Implements/Converters/StringValueConverter.cs
+++ Feast.Foundation/Feast.Foundation.Core/Implements/Converters/StringValueConverter.cs
@@ -1,6 +1,6 @@
-﻿using System.Reflection;
-using Feast.Foundation.Core.Extensions;
+﻿using Feast.Foundation.Core.Extensions;
 using Feast.Foundation.Core.Interface.Converting;
+using System.Reflection;
 
 namespace Feast.Foundation.Core.Implements.Converters
 {
@@ -8,36 +8,34 @@ namespace Feast.Foundation.Core.Implements.Converters
     {
         private static readonly Dictionary<Type, StringValueConverter> Converters = new();
 
-        public static IValueConverter ConvertTo(Type toType)
-        {
-            return Converters.TryGetValue(toType, out var converter)
+        public static IValueConverter ConvertTo(Type toType) =>
+            Converters.TryGetValue(toType, out var converter)
                 ? converter
                 : new StringValueConverter(toType);
-        }
-        protected static void Instantiate(StringValueConverter converter)
-        {
+        protected static void Instantiate(StringValueConverter converter) => 
             Converters.TryAdd(converter.TargetType, converter);
-        }
     }
 
     internal class StringValueConverter : StringConverterBase, IValueConverter
     {
+        private static readonly IEnumerable<MethodInfo> Extensions =
+            typeof(StringExtension)
+                .GetMethods(BindingFlags.Static | BindingFlags.Public)
+                .Where(x => x.GetParameters().Length == 1
+                            && x.GetParameters()[0].ParameterType == typeof(string)
+                            && x.Name.StartsWith("To"));
+
         public readonly Type TargetType;
+        
         public StringValueConverter(Type toType)
         {
             TargetType = toType;
             Instantiate(this);
-            var method = typeof(StringExtension)
-                             .GetMethods(BindingFlags.Static | BindingFlags.Public)
-                             .FirstOrDefault(x =>
-                                 x.GetParameters().Length == 1
-                                 && x.GetParameters()[0].ParameterType == typeof(string)
-                                 && x.Name.StartsWith("To")
-                                 && x.ReturnType == toType)
+            var method = Extensions.FirstOrDefault(x => x.ReturnType == toType)
                          ?? throw new NotSupportedException(
                              $"{toType.Name} convert from {nameof(String)} was not supported");
             var invoker = method.CreateInvoker();
-            Converter = s => invoker.Invoker.Invoke(null, new object[]{ s })!;
+            Converter = s => invoker.Invoker.Invoke(null, s);
         }
 
         protected readonly Func<object?, object> Converter;
