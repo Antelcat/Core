400dd243a0ab5fc6ab383cf0503abbc53b49bfbd
Feast <feast107@gmail.com>
2023-05-15 23:33:45 +0800

    Create ViewModel.cs

---
+++ Feast.Foundation/Feast.Foundation.Wpf/ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Media3D;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Feast.Foundation.Core.Extensions;
+
+namespace Feast.Foundation.Wpf
+{
+    public partial class ViewModel : ObservableObject
+    {
+        [ObservableProperty] private string? message;
+
+        public int Index => index++;
+        private int index;
+        private Task task;
+        private CancellationTokenSource? canceler;
+
+        private void ShowMessage()
+        {
+            canceler?.Cancel();
+            Message = $"This is message {Index}";
+            var c = canceler = new();
+            Task.Run(async() =>
+            {
+                await 3000;
+                if (c.Token.IsCancellationRequested) return;
+                Message = null;
+            }, c.Token);
+        }
+
+        [RelayCommand]
+        private async Task Show()
+        {
+            ShowMessage();
+            await 1;
+        }
+    }
+}
