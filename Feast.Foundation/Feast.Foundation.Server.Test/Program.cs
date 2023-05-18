29ca880791d9455cfd78bb7a3d6c356cab975e07
Feast <feast107@gmail.com>
2023-05-18 23:30:37 +0800

    Update Program.cs

--- Feast.Foundation/Feast.Foundation.Server.Test/Program.cs
+++ Feast.Foundation/Feast.Foundation.Server.Test/Program.cs
@@ -1,4 +1,6 @@
 
+using Feast.Foundation.Core.Extensions;
+using Feast.Foundation.Core.Interface.Logging;
 using Feast.Foundation.Server.Extensions;
 using Feast.Foundation.Server.Test.Models;
 
@@ -10,9 +12,14 @@ namespace Feast.Foundation.Server.Test
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddFeastLogger();
             // Add services to the container.
-            builder.Services.ConfigureJwt<User>((p) => { });
-            builder.Services.AddControllers();
+            builder.Services
+                .AddControllers()
+                .AddControllersAsServices()
+                .UseAutowiredControllers();
+            builder.Services.ConfigureJwt<User>((p) => { }, failed: (c) =>
+                @"{ ""?"" : ""?"" }");
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddJwtSwaggerGen();
