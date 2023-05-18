2e470b972d80a9cc2fada5c93ba2931e0050fe09
Feast <feast107@gmail.com>
2023-05-18 23:30:34 +0800

    Update HomeController.cs

--- Feast.Foundation/Feast.Foundation.Server.Test/Controllers/HomeController.cs
+++ Feast.Foundation/Feast.Foundation.Server.Test/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Feast.Foundation.Core.Models;
+﻿using Feast.Foundation.Core.Extensions;
+using Feast.Foundation.Core.Models;
+using Feast.Foundation.Server.Controllers;
 using Feast.Foundation.Server.Test.Models;
 using Feast.Foundation.Server.Utils;
 using Microsoft.AspNetCore.Authorization;
@@ -8,8 +10,9 @@ namespace Feast.Foundation.Server.Test.Controllers
 {
     [ApiController]
     [Route("[controller]/[action]")]
-    [AllowAnonymous]
-    public class HomeController : Controller
+    [Authorize]
+
+    public class HomeController : FeastController<User>
     {
         private JwtConfigure<User> configure;
 
@@ -19,9 +22,17 @@ namespace Feast.Foundation.Server.Test.Controllers
         }
 
         [HttpPost]
+        [AllowAnonymous]
         public Response<string> Login([FromBody]User user)
         {
-            return new Response<string>(configure.CreateToken(user));
+            return new Response<string>(configure.CreateToken(user)!);
+        }
+        
+        [HttpPost]
+        public Response<User> WhoAmI()
+        {
+            Logger.LogTrace($"{Identity}");
+            return Identity;
         }
     }
 }
