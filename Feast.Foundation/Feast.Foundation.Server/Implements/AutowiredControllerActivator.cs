8e4b84e3adb66a57402727502e2c6b10fc031b0c
Feast <feast107@gmail.com>
2023-05-18 23:30:49 +0800

    Create AutowiredControllerActivator.cs

---
+++ Feast.Foundation/Feast.Foundation.Server/Implements/AutowiredControllerActivator.cs
@@ -0,0 +1,27 @@
+﻿using Feast.Foundation.Core.Implements.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Feast.Foundation.Server.Implements;
+
+public class AutowiredControllerActivator : IControllerActivator
+{
+    public object Create(ControllerContext? context)
+    {
+        if (context == null)
+            throw new ArgumentNullException($"{nameof(ControllerContext)} is null");
+        var type = context.ActionDescriptor.ControllerTypeInfo.AsType();
+        var provider = context.HttpContext.RequestServices;
+        if (provider is not AutowiredServiceProvider)
+            provider = new AutowiredServiceProvider(context.HttpContext.RequestServices);
+        return provider.GetRequiredService(type);
+    }
+
+    public void Release(ControllerContext? context, object? controller)
+    {
+        if (context == null) throw new ArgumentNullException($"{nameof(ControllerContext)} is null");
+        if (controller == null) throw new ArgumentNullException(nameof(controller));
+        if (controller is IDisposable disposable) disposable.Dispose();
+    }
+}
