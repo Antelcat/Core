19783c6c27def7b60e70fc0ceb9cd6be04365298
Feast <feast107@gmail.com>
2023-05-18 23:30:42 +0800

    Update FeastController.cs

--- Feast.Foundation/Feast.Foundation.Server/Controllers/FeastController.cs
+++ Feast.Foundation/Feast.Foundation.Server/Controllers/FeastController.cs
@@ -1,4 +1,6 @@
-﻿using Feast.Foundation.Server.Extensions;
+﻿using Feast.Foundation.Core.Attributes;
+using Feast.Foundation.Core.Interface.Logging;
+using Feast.Foundation.Server.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Feast.Foundation.Server.Controllers
@@ -7,9 +9,12 @@ namespace Feast.Foundation.Server.Controllers
     {
         protected TIdentity Identity<TIdentity>() => (TIdentity)(identity ??= JwtExtension<TIdentity>.FromClaims(User.Claims)!);
         private object? identity;
-    }
 
-    public abstract class FeastController<TIdentity> : Controller
+        [Autowired] protected IFeastLogger<FeastController> Logger { get; init; } = null!;
+    }
+    
+    
+    public abstract class FeastController<TIdentity> : FeastController
     {
         protected TIdentity Identity => identity ??= JwtExtension<TIdentity>.FromClaims(User.Claims);
         private TIdentity? identity;
