fc1347d4a01778dde5a713cce4ed7d43da5a80dc
Feast <feast107@gmail.com>
2023-05-18 12:27:14 +0800

    Update Global.cs

--- Feast.Foundation/Feast.Foundation.Core/Global.cs
+++ Feast.Foundation/Feast.Foundation.Core/Global.cs
@@ -4,6 +4,6 @@ namespace Feast.Foundation.Core
 {
     public static class Global
     {
-        public static Encoding Encoding = Encoding.UTF8;
+        public static Encoding Encoding { get; set; } = Encoding.UTF8;
     }
 }
