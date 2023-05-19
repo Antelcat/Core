1850a44237dc77209bac01b2c58f2daf3d8c139c
Feast <feast107@gmail.com>
2023-05-19 17:31:18 +0800

    Create ILSetter.cs

---
+++ Feast.Foundation/Feast.Foundation.Core/Structs/IL/ILSetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Feast.Foundation.Core.Extensions;
+using Feast.Foundation.Core.Interface.IL;
+
+namespace Feast.Foundation.Core.Structs.IL;
+
+public readonly struct ILSetter : ILMethod
+{
+    public readonly MemberInfo Method;
+    private readonly Setter<object, object> setter;
+
+    public ILSetter(PropertyInfo prop)
+    {
+        Method = prop;
+        setter = prop.CreateSetter();
+    }
+    public ILSetter(FieldInfo field)
+    {
+        Method = field;
+        setter = field.CreateSetter();
+    }
+
+    public static implicit operator ILSetter(PropertyInfo prop) => new ILSetter(prop);
+    public static implicit operator ILSetter(FieldInfo field) => new ILSetter(field);
+
+    public object? Invoke(object? target, params object?[]? parameters)
+    {
+        setter(ref target, parameters?.Length > 0 ? parameters[0] : null);
+        return null;
+    }
+}
