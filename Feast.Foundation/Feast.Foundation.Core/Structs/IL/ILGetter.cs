1d3a07ea130362bdb118870af80e8a38137f1b33
Feast <feast107@gmail.com>
2023-05-19 17:31:17 +0800

    Create ILGetter.cs

---
+++ Feast.Foundation/Feast.Foundation.Core/Structs/IL/ILGetter.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using Feast.Foundation.Core.Extensions;
+using Feast.Foundation.Core.Interface.IL;
+
+namespace Feast.Foundation.Core.Structs.IL;
+
+public readonly struct ILGetter : ILMethod
+{
+    public readonly MemberInfo Method;
+    private readonly Getter<object, object> getter;
+
+    public ILGetter(PropertyInfo prop)
+    {
+        Method = prop;
+        getter = prop.CreateGetter();
+    }
+    public ILGetter(FieldInfo field)
+    {
+        Method = field;
+        getter = field.CreateGetter();
+    }
+
+    public static implicit operator ILGetter(PropertyInfo prop) => new ILGetter(prop);
+    public static implicit operator ILGetter(FieldInfo field) => new ILGetter(field);
+
+
+    public object? Invoke(object? target, params object?[]? parameters) => 
+        getter.Invoke(target);
+}
