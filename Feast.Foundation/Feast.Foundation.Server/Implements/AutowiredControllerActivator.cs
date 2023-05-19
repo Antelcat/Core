01e06ab0367138479888f2b3c6a93eeae60d7495
Feast <feast107@gmail.com>
2023-05-19 09:08:52 +0800

    Update AutowiredControllerActivator.cs

--- Feast.Foundation/Feast.Foundation.Server/Implements/AutowiredControllerActivator.cs
+++ Feast.Foundation/Feast.Foundation.Server/Implements/AutowiredControllerActivator.cs
@@ -1,11 +1,12 @@
-﻿using Feast.Foundation.Core.Implements.Services;
+﻿using Feast.Foundation.Core.Attributes;
+using Feast.Foundation.Core.Implements.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Feast.Foundation.Server.Implements;
 
-public class AutowiredControllerActivator : IControllerActivator
+public class AutowiredControllerActivator<TAttribute> : IControllerActivator where TAttribute : Attribute
 {
     public object Create(ControllerContext? context)
     {
@@ -13,8 +14,8 @@ public class AutowiredControllerActivator : IControllerActivator
             throw new ArgumentNullException($"{nameof(ControllerContext)} is null");
         var type = context.ActionDescriptor.ControllerTypeInfo.AsType();
         var provider = context.HttpContext.RequestServices;
-        if (provider is not AutowiredServiceProvider)
-            provider = new AutowiredServiceProvider(context.HttpContext.RequestServices);
+        if (provider is not AutowiredServiceProvider<TAttribute>)
+            provider = new AutowiredServiceProvider<TAttribute>(context.HttpContext.RequestServices);
         return provider.GetRequiredService(type);
     }
 
@@ -25,3 +26,5 @@ public class AutowiredControllerActivator : IControllerActivator
         if (controller is IDisposable disposable) disposable.Dispose();
     }
 }
+
+public class AutowiredControllerActivator :  AutowiredControllerActivator<AutowiredAttribute>{ }
