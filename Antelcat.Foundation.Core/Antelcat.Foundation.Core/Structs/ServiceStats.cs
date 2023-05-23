5901a852bd92041f5393166659b11637b0cd1072
Feast <feast107@gmail.com>
2023-05-23 23:48:28 +0800

    Create ServiceStats.cs

---
+++ Antelcat.Foundation.Core/Antelcat.Foundation.Core/Structs/ServiceStats.cs
@@ -0,0 +1,20 @@
+﻿using Antelcat.Foundation.Core.Extensions;
+
+namespace Antelcat.Foundation.Core.Structs;
+
+public class ServiceStats
+{
+    public Dictionary<Type, List<Tuple<Type, Setter<object, object>>>> CachedMappers { get; private init; }= new();
+    public List<Type> ResolvedSingletons { get; private init; } = new();
+    public List<Type> ResolvedScopes { get; } = new();
+
+    public bool IsResolved(Type type) => ResolvedScopes.Contains(type) || ResolvedSingletons.Contains(type);
+    public ServiceStats CreateScope()
+    {
+        return new ServiceStats
+        {
+            CachedMappers = CachedMappers,
+            ResolvedSingletons = ResolvedSingletons
+        };
+    }
+}
