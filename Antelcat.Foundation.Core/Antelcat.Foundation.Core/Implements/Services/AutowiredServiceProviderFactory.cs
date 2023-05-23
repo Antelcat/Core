a64021a3b289508068250b1cb5ad749e0b8b0f77
Feast <feast107@gmail.com>
2023-05-23 23:48:24 +0800

    Update AutowiredServiceProviderFactory.cs

--- Antelcat.Foundation.Core/Antelcat.Foundation.Core/Implements/Services/AutowiredServiceProviderFactory.cs
+++ Antelcat.Foundation.Core/Antelcat.Foundation.Core/Implements/Services/AutowiredServiceProviderFactory.cs
@@ -1,34 +1,45 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Antelcat.Foundation.Core.Attributes;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Antelcat.Foundation.Core.Implements.Services;
 
-public abstract class BaseServiceProviderFactory<TServiceProvider> : IServiceProviderFactory<IServiceCollection> 
+public abstract class BaseServiceProviderFactory<TServiceProvider> 
+    : IServiceProviderFactory<IServiceCollection> 
 where TServiceProvider : IServiceProvider
 {
     protected abstract TServiceProvider ProvideService(IServiceCollection collection);
-    
+
     public IServiceCollection CreateBuilder(IServiceCollection? services) => services ?? new ServiceCollection();
 
-    public IServiceProvider CreateServiceProvider(IServiceCollection containerBuilder) => ProvideService(containerBuilder);
+    public IServiceProvider CreateServiceProvider(IServiceCollection containerBuilder) => 
+        ProvideService(containerBuilder);
 }
 
-public class AutowiredServiceProviderFactory<TAttribute> : BaseServiceProviderFactory<AutowiredServiceProvider<TAttribute>>
+public class AutowiredServiceProviderFactory<TAttribute> 
+    : BaseServiceProviderFactory<AutowiredServiceProvider<TAttribute>>
     where TAttribute : Attribute
 {
     private readonly Func<IServiceCollection, IServiceProvider> builder;
 
-    public AutowiredServiceProviderFactory(Func<IServiceCollection, IServiceProvider> builder) => this.builder = builder;
+    public AutowiredServiceProviderFactory(Func<IServiceCollection, IServiceProvider> builder) =>
+        this.builder = builder;
 
-    protected override AutowiredServiceProvider<TAttribute> ProvideService(IServiceCollection collection) => new(builder(collection));
+    protected override AutowiredServiceProvider<TAttribute> ProvideService(IServiceCollection collection) =>
+        new(builder(collection), collection);
 }
 
 
-public class CachedAutowiredServiceProviderFactory<TAttribute> : BaseServiceProviderFactory<CachedAutowiredServiceProvider<TAttribute>>
-    where TAttribute : Attribute
+public class AutowiredServiceScopeFactory : IServiceScopeFactory
 {
-    private readonly Func<IServiceCollection, IServiceProvider> builder;
-    
-    public CachedAutowiredServiceProviderFactory(Func<IServiceCollection, IServiceProvider> builder) => this.builder = builder;
+    private readonly IServiceScopeFactory proxy;
+    private readonly Func<IServiceProvider, IServiceProvider> provider;
+    public AutowiredServiceScopeFactory(IServiceScopeFactory factory,
+        Func<IServiceProvider,IServiceProvider> provider)
+    {
+        proxy = factory;
+        this.provider = provider;
+    }
 
-    protected override CachedAutowiredServiceProvider<TAttribute> ProvideService(IServiceCollection collection) => new(builder(collection));
+    public IServiceScope CreateScope() => new AutowiredServiceScope(proxy.CreateScope(), provider);
 }
+
