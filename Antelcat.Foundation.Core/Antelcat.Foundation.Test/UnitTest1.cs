39351d894bd1000bee233093d4d7f43457d9eacb
Feast <feast107@gmail.com>
2023-05-25 10:10:25 +0800

    Update UnitTest1.cs

--- Antelcat.Foundation.Core/Antelcat.Foundation.Test/UnitTest1.cs
+++ Antelcat.Foundation.Core/Antelcat.Foundation.Test/UnitTest1.cs
@@ -123,49 +123,7 @@ namespace Feast.Foundation.Test
             var d3 = scope2.ServiceProvider.GetRequiredService<ID>();
         }
 
-        public interface IA { }
-        public class A : IA
-        {
-            public A(IB b)
-            {
-                
-            }
-            private static int Count = 0;
-            private readonly int Number = ++Count;
-            [Autowired]
-            private IB B { get; set; }
-        }
-        public interface IB { }
-        public class B : IB
-        {
-            private static int Count = 0;
-            private readonly int Number = ++Count;
-            [Autowired]
-            private IA A { get; set; }
-        }
-        public interface IC { }
-        public class C : IC
-        {
-            private static int Count = 0;
-            private readonly int Number = ++Count;
-            [Autowired]
-            private readonly IA A;
-            [Autowired]
-            private readonly IB B;
-        }
-        public interface ID { }
-        public class D : ID
-        {
-            private static int Count = 0;
-            private readonly int Number = ++Count;
-            [Autowired]
-            private readonly IA A;
-            [Autowired]
-            private readonly IB B;
-            [Autowired]
-            private readonly IC C;
-        }
-        
+     
         [Test]
         public void RunSync()
         {
