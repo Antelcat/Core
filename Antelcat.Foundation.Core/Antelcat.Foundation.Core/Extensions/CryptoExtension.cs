571cf7ed71c7bb86dd2ceb9f8a7161f9d267c748
Feast <feast107@gmail.com>
2023-05-26 23:41:12 +0800

    Update CryptoExtension.cs

--- Antelcat.Foundation.Core/Antelcat.Foundation.Core/Extensions/CryptoExtension.cs
+++ Antelcat.Foundation.Core/Antelcat.Foundation.Core/Extensions/CryptoExtension.cs
@@ -1,8 +1,7 @@
-﻿namespace Antelcat.Foundation.Core.Extensions
+﻿namespace Antelcat.Foundation.Core.Extensions;
+
+public static class CryptoExtension
 {
-    public static class CryptoExtension
-    {
-        public static string Base64Encrypt(this string source) => Convert.ToBase64String(Global.Encoding.GetBytes(source));
-        public static string Base64Decrypt(string base64) => Global.Encoding.GetString(Convert.FromBase64String(base64));
-    }
+    public static string Base64Encrypt(this string source) => Convert.ToBase64String(Global.Encoding.GetBytes(source));
+    public static string Base64Decrypt(string base64) => Global.Encoding.GetString(Convert.FromBase64String(base64));
 }
