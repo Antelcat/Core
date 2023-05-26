6293f92a2f8e3f3092aee8fdc2d38b4c8ca9964e
Feast <feast107@gmail.com>
2023-05-26 23:41:01 +0800

    Update PathArgAttribute.cs

--- Antelcat.Foundation.Core/Antelcat.Foundation.Core/Attributes/PathArgAttribute.cs
+++ Antelcat.Foundation.Core/Antelcat.Foundation.Core/Attributes/PathArgAttribute.cs
@@ -1,14 +1,13 @@
 ﻿using Antelcat.Foundation.Core.Implements;
 
-namespace Antelcat.Foundation.Core.Attributes
+namespace Antelcat.Foundation.Core.Attributes;
+
+/// <summary>
+/// 使用 <see cref="PathResolver"/> 对该注解进行解析
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class PathArgAttribute : Attribute
 {
-    /// <summary>
-    /// 使用 <see cref="PathResolver"/> 对该注解进行解析
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Property)]
-    public class PathArgAttribute : Attribute
-    {
-        public readonly string? Name;
-        public PathArgAttribute(string? name = null) => Name = name;
-    }
+    public readonly string? Name;
+    public PathArgAttribute(string? name = null) => Name = name;
 }
