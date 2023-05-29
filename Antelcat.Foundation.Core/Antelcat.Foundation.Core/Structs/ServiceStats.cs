e21971ee64f3ffb0df847a1e8f83df2dc6230410
Feast <feast107@gmail.com>
2023-05-29 22:36:37 +0800

    Update ServiceStats.cs

--- Antelcat.Foundation.Core/Antelcat.Foundation.Core/Structs/ServiceStats.cs
+++ Antelcat.Foundation.Core/Antelcat.Foundation.Core/Structs/ServiceStats.cs
@@ -4,17 +4,45 @@ namespace Antelcat.Foundation.Core.Structs;
 
 public class ServiceStats
 {
-    public Dictionary<Type, List<Tuple<Type, Setter<object, object>>>> CachedMappers { get; private init; }= new();
-    public List<Type> ResolvedSingletons { get; private init; } = new();
-    public List<Type> ResolvedScopes { get; } = new();
+    
+    /// <summary>
+    /// 缓存的实现类的属性字段映射器
+    /// </summary>
+    public Dictionary<Type, ServiceStat> CachedMappers { get; private init; }= new();
+    
+    /// <summary>
+    /// 解析过的单例
+    /// </summary>
+    public HashSet<Type> ResolvedSingletons { get; init; } = new();
+    
+    /// <summary>
+    /// 解析过的会话
+    /// </summary>
+    public HashSet<Type> ResolvedScopes { get; } = new();
 
-    public bool IsResolved(Type type) => ResolvedScopes.Contains(type) || ResolvedSingletons.Contains(type);
+    /// <summary>
+    /// 判断是否解析过
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+    public bool IsResolved(Type type) => ResolvedSingletons.Contains(type) || ResolvedScopes.Contains(type);
+    
+    /// <summary>
+    /// 创建一个Scope
+    /// </summary>
+    /// <returns></returns>
     public ServiceStats CreateScope()
     {
         return new ServiceStats
         {
             CachedMappers = CachedMappers,
-            ResolvedSingletons = ResolvedSingletons
+            ResolvedSingletons = ResolvedSingletons,
         };
     }
+}
+
+public struct ServiceStat
+{
+    public bool NeedAutowired { get; init; }
+    public List<Tuple<Type, Setter<object, object>>>? Mappers;
 }
