9879c2476144e96724cd27490b962ba9ff1d0e68
Feast <feast107@gmail.com>
2023-05-29 23:04:43 +0800

    Update TestClasses.cs

--- Antelcat.Foundation.Core/Antelcat.Foundation.Test/TestClasses.cs
+++ Antelcat.Foundation.Core/Antelcat.Foundation.Test/TestClasses.cs
@@ -2,42 +2,38 @@
 
 namespace Feast.Foundation.Test;
 
+public abstract class Factory<T> where T : Factory<T>
+{
+    private static int count = 0;
+    protected readonly int Number = ++count;
+}
+
 public interface IA { }
-public class A : IA
+
+public class A : Factory<A>, IA
 {
-    public A(IB b)
-    {
-        B = b;
-    }
-    private static int count = 0;
-    private readonly int number = ++count;
-    [Autowired]
-    public IB B { get; set; }
+    [Autowired] public IB B { get; set; }
 }
+
 public interface IB { }
-public class B : IB
+
+public class B : Factory<B>, IB
 {
-    private static int count = 0;
-    private readonly int number = ++count;
-    [Autowired]
-    private IA A { get; set; }
+    [Autowired] private IA A { get; set; }
 }
+
 public interface IC { }
-public class C : IC
+
+public class C : Factory<C>, IC
 {
-    private static int count = 0;
-    private readonly int number = ++count;
-    [Autowired]
-    private readonly IA A;
-    [Autowired]
-    private readonly IB B;
+    [Autowired] private readonly IA A;
+    [Autowired] private readonly IB B;
 }
+
 public interface ID { }
 
-public class D : ID
+public class D : Factory<D>, ID
 {
-    private static int count = 0;
-    private readonly int number = ++count;
     [Autowired] private readonly IA A;
     [Autowired] private readonly IB B;
     [Autowired] private readonly IC C;
