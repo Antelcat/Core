a554505ae50ea1d4f3a62ccb14d0965416f1f574
Feast <feast107@gmail.com>
2023-05-30 10:12:32 +0800

    Update TestClasses.cs

--- Antelcat.Foundation.Core/Antelcat.Foundation.Test/TestClasses.cs
+++ Antelcat.Foundation.Core/Antelcat.Foundation.Test/TestClasses.cs
@@ -34,11 +34,17 @@ public interface ID { }
 
 public class D : Factory<D>, ID
 {
-    [Autowired] private readonly IA A;
+    public D(IA a)
+    {
+        A = a;
+    }
+    private readonly IA A;
     [Autowired] private readonly IB B;
-    [Autowired] private readonly IC C;
 }
 
-public class GenericType<T>
-{
-}
+public interface IGeneric<T> { }
+
+public interface IMultiGeneric<T1, T2, T3> { }
+
+public class GenericType<T> : IGeneric<T> { }
+public class MultiGenericType<T1,T2,T3> : IMultiGeneric<T1,T2,T3> { }
