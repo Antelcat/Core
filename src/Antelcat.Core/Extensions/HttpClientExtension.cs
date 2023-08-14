a8ede58de1c0b8efd849eb2ab4739fccf5cb1872
Feast <feast107@gmail.com>
2023-08-14 13:54:29 +0800

    Create HttpClientExtension.cs

---
+++ src/Antelcat.Core/Extensions/HttpClientExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using Antelcat.Extensions;
+
+namespace Antelcat.Core.Extensions;
+
+public static class HttpClientExtension
+{
+    public static Task<string> GetStringAsync(this HttpClient client,string requestUrl, Dictionary<string,string>? args)
+    {
+        var sb = new StringBuilder($"{requestUrl}?");
+        args?.ForEach(kv => sb.Append($"{kv.Key}={kv.Value}&"));
+        sb.Remove(sb.Length - 1, 1);
+        return client.GetStringAsync(sb.ToString());
+    }
+}
