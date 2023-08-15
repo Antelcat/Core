b0fb5463d13f0a40ab133ee01b90da092841f928
Feast <feast107@gmail.com>
2023-08-16 00:28:34 +0800

    Update HttpClientExtension.cs

--- src/Antelcat.Core/Extensions/HttpClientExtension.cs
+++ src/Antelcat.Core/Extensions/HttpClientExtension.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net.Http.Json;
+using System.Text;
+using Antelcat.Core.Models;
 using Antelcat.Extensions;
 
 namespace Antelcat.Core.Extensions;
@@ -7,9 +9,29 @@ public static class HttpClientExtension
 {
     public static Task<string> GetStringAsync(this HttpClient client,string requestUrl, Dictionary<string,string>? args)
     {
-        var sb = new StringBuilder($"{requestUrl}?");
+        if (args != null) requestUrl = MergeArgs(requestUrl, args);
+        return client.GetStringAsync(requestUrl);
+    }
+    
+    public static async Task<string> PutStringAsync(this HttpClient client,
+        string requestUrl, 
+        Dictionary<string,string>? args,
+        object? json)
+    {
+        if (args != null) requestUrl = MergeArgs(requestUrl, args);
+        return await (
+                await client.PutAsync(
+                    requestUrl,
+                    new StringContent(json?.Serialize() ?? string.Empty)))
+            .Content
+            .ReadAsStringAsync();
+    }
+
+    private static string MergeArgs(string prefix,Dictionary<string,string> args)
+    {
+        var sb = new StringBuilder($"{prefix}?");
         args?.ForEach(kv => sb.Append($"{kv.Key}={kv.Value}&"));
         sb.Remove(sb.Length - 1, 1);
-        return client.GetStringAsync(sb.ToString());
+        return sb.ToString();
     }
 }
